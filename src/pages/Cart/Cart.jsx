5c943769cf75103474685220fd9a84b35179b991
Shohruh174 <shbotirov227@gmail.com>
2022-03-02 19:00:16 +0500

    table

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -4,100 +4,62 @@ import Footer from "../../containers/Footer/Footer";
 import Header from "../../containers/Header/Header";
 import makeData from "./makeData";
 
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Img1 from "../../assets/images/saleCardBg3.png"
+
 import "./Cart.scss";
 
-function Table({ columns, data }) {
-	// Use the state and functions returned from useTable to build your UI
-	const {
-	  getTableProps,
-	  getTableBodyProps,
-	  headerGroups,
-	  rows,
-	  prepareRow,
-	} = useTable({
-	  columns,
-	  data,
-	})
-  
-	// Render the UI for your table
-	return (
-	  <table {...getTableProps()}>
-		<thead>
-		  {headerGroups.map(headerGroup => (
-			<tr {...headerGroup.getHeaderGroupProps()}>
-			  {headerGroup.headers.map(column => (
-				<th {...column.getHeaderProps()}>{column.render('Header')}</th>
-			  ))}
-			</tr>
-		  ))}
-		</thead>
-		<tbody {...getTableBodyProps()}>
-		  {rows.map((row, i) => {
-			prepareRow(row)
-			return (
-			  <tr {...row.getRowProps()}>
-				{row.cells.map(cell => {
-				  return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-				})}
-			  </tr>
-			)
-		  })}
-		</tbody>
-	  </table>
-	)
-}
+const getimg = (img) => <img src={img} width="135" height="95" alt="" />
 
+function createData(img, title, price, unitPrice) {
+	return {img, title, price, unitPrice}
+}
+  
+  const rows = [
+	createData(getimg(Img1), "Nike Airmax 270 react", "$998", "$499"),
+	createData(getimg(Img1), "Nike Airmax 270 react", "$998", "$499"),
+  ];
 
 const Cart = () => {
-
-	const columns = React.useMemo( () => [
-			{
-				Header: 'Name',
-				columns: [
-				{
-					Header: 'Product',
-					accessor: 'product',
-				},
-				{
-					Header: 'Last Name',
-					accessor: 'lastName',
-				},
-				],
-			},
-
-			{
-				Header: 'Info',
-				columns: [
-				{
-					Header: 'Age',
-					accessor: 'age',
-				},
-				{
-					Header: 'Price',
-					accessor: 'price',
-				},
-				{
-					Header: 'QTY',
-					accessor: 'status',
-				},
-				{
-					Header: 'UNIT PRICE',
-					accessor: 'unitPrice',
-				},
-				],
-			},
-
-		], []
-	  )
-
-	const data = React.useMemo(() => makeData(20), []);
 
 	return (
 		<div className="Cart">
 			<Header />
-			<div className="Cart-inner container">
-				<div className="Cart-tableStyles">
-					<Table columns={columns} data={data} />
+			<div className="container">
+				<div className="Cart-inner">
+					<div className="Cart-tableStyles">
+					<TableContainer component={Paper}>
+						<Table sx={{ minWidth: 650 }} aria-label="simple table">
+							<TableHead>
+							<TableRow>
+								<TableCell>PRODUCT</TableCell>
+								<TableCell align="right">PRICE</TableCell>
+								<TableCell align="right">QTY</TableCell>
+								<TableCell align="right">UNIT PRICE</TableCell>
+							</TableRow>
+							</TableHead>
+							<TableBody>
+							{rows.map((row) => (
+								<TableRow
+								key={row.name}
+								sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+								>
+								<TableCell align="right">{row.img}</TableCell>
+								<TableCell align="left">{row.title}</TableCell>
+								<TableCell align="right">{row.price}</TableCell>
+								<TableCell align="right">{row.unitPrice}</TableCell>
+								</TableRow>
+							))}
+							</TableBody>
+						</Table>
+						</TableContainer>
+					</div>
 				</div>
 			</div>
 			<Footer marginTop="155px" />
