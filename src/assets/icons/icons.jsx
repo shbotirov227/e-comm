728b50b1f2501c44b5de68496181c0236d2f7d1a
Shohruh174 <shbotirov227@gmail.com>
2022-03-07 18:10:04 +0500

    modalPayment

--- src/assets/icons/icons.jsx
+++ src/assets/icons/icons.jsx
@@ -91,6 +91,37 @@ const Close = () => {
     )
 }
 
+const PaymentIcon1 = () => {
+    return (
+        <svg width="21" height="17" viewBox="0 0 21 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M19.8278 1.78223H1.64941V15.2822H19.8278V1.78223Z" stroke="#40BFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M1.64941 6.28223H19.8278" stroke="#40BFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    )
+}
+
+const PaymentIcon2 = () => {
+    return (
+        <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.0619 7.41573L10.0644 7.41673C10.1169 7.19523 10.3078 7.03223 10.5466 7.03223H17.3494C17.358 7.03223 17.3666 7.02873 17.3752 7.02923C17.2302 4.63973 15.2059 3.53223 13.4198 3.53223H6.61708C6.37773 3.53223 6.18686 3.69973 6.13485 3.92073L6.13232 3.91973L3.15864 17.4387L3.1652 17.4392C3.15813 17.4712 3.14551 17.5017 3.14551 17.5362C3.14551 17.8127 3.37122 18.0317 3.65046 18.0317H7.72596L10.0619 7.41573Z" fill="#1565C0"/>
+            <path d="M17.3767 7.0293C17.4035 7.4673 17.3742 7.9438 17.2611 8.4703C16.6142 11.4678 14.2758 13.0278 11.3859 13.0278C11.3859 13.0278 9.63374 13.0278 9.20806 13.0278C8.94498 13.0278 8.82076 13.1808 8.7637 13.2978L7.88508 17.3223L7.73107 18.0368H7.72804L7.09028 20.9348L7.09684 20.9353C7.08977 20.9673 7.07715 20.9978 7.07715 21.0323C7.07715 21.3088 7.30286 21.5323 7.5821 21.5323H11.2849L11.2915 21.5273C11.5298 21.5238 11.7192 21.3553 11.7687 21.1333L11.7778 21.1258L12.6928 16.9178C12.6928 16.9178 12.7564 16.5163 13.1826 16.5163C13.6088 16.5163 15.2923 16.5163 15.2923 16.5163C18.1821 16.5163 20.5443 14.9633 21.1917 11.9653C21.9198 8.5853 19.4849 7.0418 17.3767 7.0293Z" fill="#039BE5"/>
+            <path d="M10.5477 7.03198C10.3084 7.03198 10.1175 7.19498 10.0655 7.41648L10.063 7.41548L8.7627 13.298C8.81976 13.181 8.94397 13.028 9.20706 13.028C9.63324 13.028 11.3455 13.028 11.3455 13.028C14.2354 13.028 16.6132 11.468 17.2601 8.47048C17.3737 7.94398 17.4025 7.46748 17.3757 7.02948C17.3676 7.02848 17.3586 7.03198 17.3505 7.03198H10.5477Z" fill="#283593"/>
+        </svg>
+    )
+}
+
+const PaymentIcon3 = () => {
+    return (
+        <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1.64941 4.90723L10.7386 1.53223L19.8278 4.90723" stroke="#40BFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M1.64941 19.5322H19.8278" stroke="#40BFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M10.7383 15.0322V8.28223" stroke="#40BFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M17.5557 15.0322V8.28223" stroke="#40BFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M3.92188 15.0322V8.28223" stroke="#40BFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    )
+}
+
 export {
     Logo,
     UserIcon,
@@ -103,4 +134,7 @@ export {
     MinusIcon,
     Arrow,
     Close,
+    PaymentIcon1,
+    PaymentIcon2,
+    PaymentIcon3
 }
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -10,7 +10,7 @@ import Footer from "../../containers/Footer/Footer";
 import Header from "../../containers/Header/Header";
 import Payment1 from './../../components/Payment1/Payment1';
 import Img1 from "../../assets/images/saleCardBg3.png"
-import {MinusIcon, PlusIcon, X, Arrow, Close} from "../../assets/icons/icons";
+import { MinusIcon, PlusIcon, X, Arrow, Close, PaymentIcon1, PaymentIcon2, PaymentIcon3 } from "../../assets/icons/icons";
 
 
 import "./Cart.scss";
@@ -123,6 +123,46 @@ const Cart = () => {
 												<h4 className="Payment1-step relative">2</h4>
 												<h4 className="Payment1-step">3</h4>
 											</div>
+
+											<div className="flex items-start justify-between w-[100%]">
+												<div className="Payment1-left">
+													<input type="text" placeholder="First Name" className="Payment1-left__input" />
+													<input type="email" name="email" placeholder="Email Address" className="Payment1-left__input" />
+													<h4 className="Payment1-left__title">Select Method of Payment</h4>
+
+													<label htmlFor="creditCard" className="Payment1-left__label">
+														<div className="flex items-center justify-between">
+															<PaymentIcon1 />
+															Credit Card Or Debit
+														</div>
+														<input type="checkbox" name="checkbox" id="creditCard" />
+													</label>
+
+													<label htmlFor="paypal" className="Payment1-left__label">
+														<div className="flex items-center justify-between">
+															<PaymentIcon2 />
+															Paypal
+														</div>
+														<input type="checkbox" name="checkbox" id="paypal" />
+													</label>
+													
+													<label htmlFor="bank" className="Payment1-left__label">
+														<div className="flex items-center justify-between">
+															<PaymentIcon3 />
+															Bank Transfer
+														</div>
+														<input type="checkbox" name="checkbox" id="bank" />
+													</label>	
+												</div>
+
+												<div className="Payment1-right">
+													<input type="text" placeholder="Last Name" className="Payment1-left__input" />
+													<textarea className="Payment1-left__input" name="textarea" placeholder="Address for Delivery"></textarea>
+													<input className="Payment1-left__input" type="number" name="numb" placeholder="Mobile Phone" />
+												</div>
+											</div>
+											
+											<button className="paymentBtn">Go to Payment</button>
 										</div>
 									) : undefined
 								}
